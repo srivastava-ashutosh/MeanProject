581af2314cce130f3b91ebea9a424b25fd74f6b8
Ashutosh Srivastava <ashutosh.srivastava.5986@gmail.com>
2017-02-23 12:08:09 +0530

    Delete mailer.server.routes.js

--- app/routes/mailer.server.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app, express) {
-	var mailer = require('../controllers/mailer.server.controller');
-	var router = express.Router();
-
-
-	router.post('/sendmail', mailer.SendMail);
-	app.use(config.baseApiUrl, router);
-
-}
