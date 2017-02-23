064c21db1c3aa38d64a76b91ffd1067aebacafd9
Ashutosh Srivastava <ashutosh.srivastava.5986@gmail.com>
2017-02-23 12:09:00 +0530

    Delete production.js

--- configs/env/production.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    db: 'mongodb://127.0.0.1/local',   
-    sessionSecret: 'indNIC2305',
-    securityToken: 'indNIC2305',
-    baseApiUrl: '/apis',
-    serverPort: '3001',
-    tokenExpiry: 30, // Note: in seconds!
-    adminEmails:'chandrakanta@indianic.com,chandrakanta1@indianic.com,chandrakanta2@indianic.com'
-};
